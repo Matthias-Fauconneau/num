9bd775c54c3e4d068970195e9cbd3669bc79b189
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2020-09-08 00:53:36 +0200

    Div Ratio

--- lib.rs
+++ lib.rs
@@ -64,4 +64,6 @@ impl Ratio {
 impl From<Ratio> for f32 { fn from(r: Ratio) -> Self { r.num as f32 / r.div as f32 } }
 impl std::ops::Mul<u32> for Ratio { type Output=u32; #[track_caller] fn mul(self, b: u32) -> Self::Output { div_floor(b * self.num, self.div) } }
 impl std::ops::Div<Ratio> for u32 { type Output=u32; fn div(self, r: Ratio) -> Self::Output { div_floor(self * r.div, r.num) } }
+impl std::ops::Div<Ratio> for i32 { type Output=i32; #[track_caller] fn div(self, r: Ratio) -> Self::Output { idiv_floor(self * r.div as i32, r.num) } }
 impl std::ops::Mul<f32> for Ratio { type Output=f32; fn mul(self, b: f32) -> Self::Output { b * self.num as f32 / self.div as f32 } } // loses precision
+impl std::ops::Div<Ratio> for f32 { type Output=f32; fn div(self, r: Ratio) -> Self::Output { self * r.div as f32 / r.num as f32 } } // loses precision
