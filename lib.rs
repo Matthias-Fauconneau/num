b30330769da89371ac5b63cbf8d6c0a8ca748b99
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2022-07-22 21:56:33 +0200

    Ratio*Ratio, unit

--- lib.rs
+++ lib.rs
@@ -67,7 +67,8 @@ pub fn idiv_ceil(n: i32, d: u32) -> i32 {
 }
 
 #[derive(Clone,Copy,Debug,PartialEq,Eq,Hash)] pub struct Ratio { pub num: u32, pub div: u32 }
-impl Default for Ratio { fn default() -> Self { Self{num: 1, div: 1} } }
+#[allow(non_upper_case_globals)] pub const unit : Ratio = Ratio{num: 1, div: 1};
+impl Default for Ratio { fn default() -> Self { unit } }
 impl Ratio {
 	pub fn rcp(&self) -> Ratio { Self{num: self.div, div: self.num} }
 	pub fn ceil(&self, x: u32) -> u32 { div_ceil(x * self.num, self.div) }
@@ -77,6 +78,7 @@ impl Ratio {
 impl From<Ratio> for f32 { fn from(r: Ratio) -> Self { r.num as f32 / r.div as f32 } }
 impl std::ops::Mul<u32> for Ratio { type Output=u32; fn mul(self, b: u32) -> Self::Output { div_floor(b * self.num, self.div) } }
 impl std::ops::Mul<i32> for Ratio { type Output=i32; fn mul(self, b: i32) -> Self::Output { self.ifloor(b) } }
+impl std::ops::Mul<Ratio> for Ratio { type Output=Ratio; fn mul(self, b: Ratio) -> Self::Output { Ratio{num: self.num * b.num, div: self.div * b.div} } }
 impl std::ops::Div<Ratio> for u32 { type Output=u32; #[track_caller] fn div(self, r: Ratio) -> Self::Output { div_floor(self * r.div, r.num) } }
 impl std::ops::Div<Ratio> for i32 { type Output=i32; fn div(self, r: Ratio) -> Self::Output { idiv_floor(self * r.div as i32, r.num) } }
 impl std::ops::Mul<f32> for Ratio { type Output=f32; fn mul(self, b: f32) -> Self::Output { b * self.num as f32 / self.div as f32 } } // loses precision
